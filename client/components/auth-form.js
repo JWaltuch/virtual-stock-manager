8cbde8b8ba5bcf1b58b4834b157d98a5db17502b
JWaltuch <jenna.waltuch@gmail.com>
2020-02-26 13:07:06 -0500

    chore: modified sign up form to reflect changes to user model

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -12,6 +12,14 @@ const AuthForm = props => {
   return (
     <div>
       <form onSubmit={handleSubmit} name={name}>
+        {displayName === 'Sign Up' && (
+          <div>
+            <label htmlFor="name">
+              <small>Name</small>
+            </label>
+            <input name="name" type="text" />
+          </div>
+        )}
         <div>
           <label htmlFor="email">
             <small>Email</small>
