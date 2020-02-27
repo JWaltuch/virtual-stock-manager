db2f0c91d0f003e397cb5ab1d7db46cf959a6f36
JWaltuch <jenna.waltuch@gmail.com>
2020-02-27 14:36:53 -0500

    style: removed weird dash

--- client/components/stock.js
+++ client/components/stock.js
@@ -8,7 +8,7 @@ export const Stock = props => {
 
   return (
     <div className="line-item">
-      <div className="column">{stock.symbol} -</div>
+      <div className="column">{stock.symbol}</div>
       <div className="column">{stock.totalShares} Shares</div>
       <div className="column">${stock.value / 1000}</div>
     </div>
