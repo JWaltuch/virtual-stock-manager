99a3655294dbb37e6244a38e15f53a728ed875e0
JWaltuch <jenna.waltuch@gmail.com>
2020-02-27 13:40:26 -0500

    feat: wrote reducer to handle getting and posting transactions on client side

---
+++ client/store/transaction.js
@@ -0,0 +1,62 @@
+import axios from 'axios'
+import history from '../history'
+
+/**
+ * ACTION TYPES
+ */
+const GOT_TRANSACTIONS = 'GOT_TRANSACTIONS'
+const ADDED_TRANSACTION = 'ADDED_TRANSACTION'
+
+/**
+ * INITIAL STATE
+ */
+const defaultTransactions = []
+
+/**
+ * ACTION CREATORS
+ */
+const gotTransactions = user => ({type: GOT_TRANSACTIONS, transactions})
+const addedTransaction = newTransaction => ({
+  type: ADDED_TRANSACTION,
+  newTransaction
+})
+
+/**
+ * THUNK CREATORS
+ */
+export const getTransactions = () => async dispatch => {
+  try {
+    const res = await axios.get('/api/transactions')
+    dispatch(gotTransactions(res.data || defaultTransactions))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const addTransaction = (symbol, shares, type) => async dispatch => {
+  let res
+  try {
+    res = await axios.post(`/api/transactions`, {symbol, shares, type})
+    dispatch(addedTransaction(res.data))
+    history.push('/transactions')
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultTransactions, action) {
+  switch (action.type) {
+    case GOT_TRANSACTIONS:
+      return action.transactions
+    case ADDED_TRANSACTION: {
+      let newTransactions = [...state]
+      newTransactions.push(action.newTransaction)
+      return newTransactions
+    }
+    default:
+      return state
+  }
+}
