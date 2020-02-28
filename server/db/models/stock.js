9fb914882d671d38d82a0532647914f618b593f9
JWaltuch <jenna.waltuch@gmail.com>
2020-02-28 10:22:26 -0500

    feat: updated stock model to keep track of opening price for owned stocks

--- server/db/models/stock.js
+++ server/db/models/stock.js
@@ -9,28 +9,36 @@ const Stock = db.define('stock', {
   totalShares: {
     type: Sequelize.INTEGER,
     allowNull: false
+  },
+  openingPrice: {
+    type: Sequelize.INTEGER,
+    allowNull: false
   }
 })
 
 //Class method to handle updating stock quantity if purchasing more
 //shares of the same stock
-Stock.findOrUpdate = async function(symbol, shares) {
+Stock.createOrUpdate = async function(symbol, shares, openingPrice) {
+  //Attempt to find Stock with the same symbol
   let existingStock = await Stock.findOne({
     where: {
       symbol: symbol
     }
   })
+  //If found, increase shares, and update opening price
   if (existingStock) {
     let newTotalShares = existingStock.totalShares + shares
     await Stock.update(
-      {totalShares: newTotalShares},
+      {totalShares: newTotalShares, openingPrice},
       {where: {id: existingStock.id}}
     )
     return existingStock
   } else {
+    //If not found, create a new stock with passed arguments
     return Stock.create({
       symbol,
-      shares
+      totalShares: shares,
+      openingPrice
     })
   }
 }
