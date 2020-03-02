b6a0c4efaf5845807999d516d6ee6b92269bf6ad
JWaltuch <jenna.waltuch@gmail.com>
2020-03-02 11:25:54 -0500

    feat: added clarifying comments and better naming. also, added utility function for checking if market is closeD

--- util.js
+++ util.js
@@ -2,11 +2,17 @@
 // Checks if the last updated date === today
 // Then, checks if it is after 9:45AM EST, which is when market opens
 
-const datesAreSameDay = (lastUpdate, currentDate) =>
+const updatedWhenMarketOpenedToday = (lastUpdate, currentDate) =>
   lastUpdate.getFullYear() === currentDate.getFullYear() &&
   lastUpdate.getMonth() === currentDate.getMonth() &&
   lastUpdate.getDate() === currentDate.getDate() &&
   (lastUpdate.getHours() > 9 ||
     (lastUpdate.getHours() === 9 && lastUpdate.getMinutes > 45))
 
-module.exports = datesAreSameDay
+// Utility function for checking if market is closed right now
+const isAfterMarketClose = currentDate =>
+  currentDate.getHours() < 9 ||
+  (currentDate.getHours() === 9 && currentDate.getMinutes < 45) ||
+  currentDate.getHours() > 16
+
+module.exports = {updatedWhenMarketOpenedToday, isAfterMarketClose}
