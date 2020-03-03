90c38baf25ffb619e65374791b6bdbf5769de3c0
JWaltuch <jenna.waltuch@gmail.com>
2020-03-02 21:27:59 -0500

    test: tests for stock models

---
+++ server/db/models/stock.spec.js
@@ -0,0 +1,31 @@
+/* global describe beforeEach it */
+
+const {expect} = require('chai')
+const db = require('../index')
+const Stock = db.model('stock')
+
+describe('Stock model', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('classMethods', () => {
+    describe('createOrUpdate', () => {
+      let aapl
+
+      beforeEach(async () => {
+        aapl = await Stock.createOrUpdate('aapl', 1, 2100)
+        aapl = aapl.dataValues
+      })
+
+      it('creates a new stock if a stock with the same symbol does not exist ', () => {
+        expect(aapl.totalShares).to.be.equal(1)
+      })
+
+      it('adds shares to the stock if a stock with the same symbol exists', async () => {
+        let updatedAapl = await Stock.createOrUpdate('aapl', 1, 2100)
+        expect(updatedAapl.totalShares).to.be.equal(2)
+      })
+    })
+  })
+})
