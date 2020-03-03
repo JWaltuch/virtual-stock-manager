5a5680a9693a24def9e7497a38dbdccf064e1bdb
JWaltuch <jenna.waltuch@gmail.com>
2020-03-03 12:54:06 -0500

    refactor: renamed and refactored stock component's method for dynamically setting colors, for clarity

--- client/components/stock.js
+++ client/components/stock.js
@@ -1,19 +1,22 @@
 import React from 'react'
 
+const setStockStatus = stock => {
+  if (stock.openingPrice === stock.value / stock.totalShares)
+    return 'equal-to-open'
+  if (stock.openingPrice > stock.value / stock.totalShares)
+    return 'less-than-open'
+  if (stock.openingPrice < stock.value / stock.totalShares)
+    return 'greater-than-open'
+}
+
 /**
  * COMPONENT
  */
 export const Stock = props => {
   const {stock} = props
-  let color = 'equal-to-open'
-  if (stock.openingPrice > stock.value / stock.totalShares) {
-    color = 'less-than-open'
-  } else if (stock.openingPrice < stock.value / stock.totalShares) {
-    color = 'greater-than-open'
-  }
-
+  const stockStatus = setStockStatus(stock)
   return (
-    <div className={`line-item ${color}`}>
+    <div className={`line-item ${stockStatus}`}>
       <div className="column">{stock.symbol}</div>
       <div className="column">{stock.totalShares} Shares</div>
       {/* Prices are recorded accurately but rounded for cleaner display */}
